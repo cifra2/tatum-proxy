70eb7143cb70afadc5b934c71ed16826dc068aca
František Cimmer <cimmer@czc.cz>
2025-10-02 07:31:56 +0200

    Doc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 import io.vertx.core.Vertx;
 
+/**
+ * Just for testing purposes.
+ */
 public class Main {
     public static void main(String[] args) {
         Vertx.vertx().deployVerticle(new TatumProxy());
